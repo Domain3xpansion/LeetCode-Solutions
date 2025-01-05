37f99020a1eceeadaeace4dedae2f8ef495b275b
Maikelele <85241561+Domain3xpansion@users.noreply.github.com>
2025-01-05 11:18:19 +0530

    0043-MULTIPLY-STRINGS Stats: Time: 0 ms (100%), Space: 17.7 MB (19.99%) - LeetHub

---
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -0,0 +1,8 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        inum1 = int(num1)
+        inum2 = int(num2)
+        res = inum1 * inum2
+        sres = str(res)
+        return sres
+        
