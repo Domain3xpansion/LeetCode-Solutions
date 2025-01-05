3a1d6c646969f07183d78db4f66c91956fbd9615
Maikelele <85241561+Domain3xpansion@users.noreply.github.com>
2025-01-05 11:21:02 +0530

    0043-MULTIPLY-STRINGS Stats: Time: 0 ms (100%), Space: 17.7 MB (26.09%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.py
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -1,8 +1,8 @@
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
-        inum1 = int(num1)
-        inum2 = int(num2)
-        res = inum1 * inum2
-        sres = str(res)
+        inum1 = int(num1)  #Converting from string to int
+        inum2 = int(num2)  #Converting from string to int
+        res = inum1 * inum2   
+        sres = str(res)  #Converting from int to res as the question demands
         return sres
         
