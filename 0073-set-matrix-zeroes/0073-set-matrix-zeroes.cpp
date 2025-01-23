ecabe2f1b7a52f72986f8429ec8ebeb0860fe672
Maikelele <85241561+Domain3xpansion@users.noreply.github.com>
2025-01-23 19:49:19 +0530

    0073-SET-MATRIX-ZEROES Stats: Time: 0 ms (100%), Space: 18.6 MB (76.11%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,47 +1,24 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-
         int m = matrix.size();     //stores no of rows
         int n = matrix[0].size();     //stores no of columns
-        for (int i = 0; i < m; i++) {    
-            for (int j = 0; j < n; j++) {
-                if (matrix[i][j] == 0) {
-                    markrows(matrix, i);   // O(m)
-                    markcolumns(matrix, j);   // O(n)
+        vector<int> rows(m, 0);
+        vector<int> columns(n, 0);
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(matrix[i][j] == 0){
+                    rows[i] = 1;
+                    columns[j] = 1;
                 }
             }
         }
-        //this whole function is of O((m*n)*(m+n))
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (matrix[i][j] == 'a') {
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(rows[i] || columns[j]){
                     matrix[i][j] = 0;
                 }
             }
         }
-        //this whole function is of O(m*n)
     }
-
-private:
-    void markrows(vector<vector<int>>& matrix, int i) {
-        int n = matrix[0].size();
-        for (int j = 0; j < n; j++) {
-            if (matrix[i][j] != 0) {
-                matrix[i][j] = 'a';
-            }
-        }
-    }
-
-    void markcolumns(vector<vector<int>>& matrix, int j) {
-        int m = matrix.size();
-        for (int i = 0; i < m; i++) {
-            if (matrix[i][j] != 0) {
-                matrix[i][j] = 'a';
-            }
-        }
-    }
-    //Brute force solution
-    //Total time complexity is near about n^3 which is not at all desirable
 };
