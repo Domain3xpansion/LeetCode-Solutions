defaa3177c44f96b3b02a21a48c2b4194c089dd3
Maikelele <85241561+Domain3xpansion@users.noreply.github.com>
2025-01-27 23:24:19 +0530

    0861-FLIPPING-AN-IMAGE Stats: Time: 0 ms (100%), Space: 12.5 MB (69.42%) - LeetHub

--- 0861-flipping-an-image/0861-flipping-an-image.cpp
+++ 0861-flipping-an-image/0861-flipping-an-image.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     vector<vector<int>> flipAndInvertImage(vector<vector<int>>& image) {
-        for (auto& row : image) {
-            std::reverse(row.begin(), row.end());
+        for (auto& row : image) {         //will give error if you write for(auto row: image) 
+            std::reverse(row.begin(), row.end());   //reversing each row of the 2D matrix
         }
         for (int i=0; i<image.size(); i++){
-            for(int j=0; j<image[0].size(); j++){
-                image[i][j] = image[i][j] ^ 1;
+            for(int j=0; j<image[0].size(); j++){   
+                image[i][j] = image[i][j] ^ 1;     //accessing each element of the 2D matrix
             }
         }
         return image; 
