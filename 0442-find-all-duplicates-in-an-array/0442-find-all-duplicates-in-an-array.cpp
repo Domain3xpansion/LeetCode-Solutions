622a5a69112c027f1c3a78b4a1f13ef4f412402a
Maikelele <85241561+Domain3xpansion@users.noreply.github.com>
2025-01-29 08:36:49 +0530

    0442-FIND-ALL-DUPLICATES-IN-AN-ARRAY Stats: Time: 18 ms (37.16%), Space: 37.7 MB (74.83%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
@@ -6,6 +6,7 @@ public:
         for(int i=0; i<nums.size()-1; i++){
             if(nums[i] == nums[i+1]){
                 if (emp.empty() || emp.back() != nums[i]) {
+                    /*emp.back() != nums[i] because there can be a case when the 2 elements being compared are equal but that element is already pushed to the solution array due to prior operations*/
                     emp.push_back(nums[i]);
                 }
             }
