95959e8dca41e3ff2ee337736982f30d2c911a44
Maikelele <85241561+Domain3xpansion@users.noreply.github.com>
2025-01-30 22:09:33 +0530

    0442-FIND-ALL-DUPLICATES-IN-AN-ARRAY Stats: Time: 42 ms (15.24%), Space: 51.9 MB (6.65%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     vector<int> findDuplicates(vector<int>& nums) {
         /*vector<int> emp;
-        std::sort(nums.begin(), nums.end());
-        for(int i=0; i<nums.size()-1; i++){
+        std::sort(nums.begin(), nums.end());     // Sorts in O(nlogn)
+        for(int i=0; i<nums.size()-1; i++){      // T.C. for this loop: O(n).
             if(nums[i] == nums[i+1]){
                 if (emp.empty() || emp.back() != nums[i]) {
                     //emp.back() != nums[i] because there can be a case when the 2 elements 
@@ -14,19 +14,21 @@ public:
             }
         }
         return emp;
+        //  Overall T.C.: O(nlogn) + O(n) = O(nlogn)
+        //  S.C.: O(n)
         */
-
         
         vector<int> emp;
         unordered_map<long, long> hm;
-        for(int number : nums){
+        for(int number : nums){      // T.C. for this loop: O(n)
             hm[number]++;
         }
         for(const auto& pair : hm){
-            if(pair.second > 1){
-                emp.push_back(pair.first);
-            }
+            if(pair.second > 1){    // If there are m unique elements, this loop runs in O(m)
+                emp.push_back(pair.first);    //Since m can be at most n,  
+            }                       // the T.C. for this loop: O(m), which can be up to O(n)
         }
         return emp;
+        //   Overall T.C.: O(n) + O(m) = O(n)
     }
 };
