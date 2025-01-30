1bf5f02316b6c710a9388ae0935ec1fdfe37da2b
Maikelele <85241561+Domain3xpansion@users.noreply.github.com>
2025-01-31 01:11:24 +0530

    0782-JEWELS-AND-STONES Stats: Time: 0 ms (100%), Space: 49.4 MB (70.4%) - LeetHub

--- 0782-jewels-and-stones/0782-jewels-and-stones.js
+++ 0782-jewels-and-stones/0782-jewels-and-stones.js
@@ -5,10 +5,13 @@
  */
 var numJewelsInStones = function(jewels, stones) {
     let countjewels = 0;
-    for(let i = 0; i < stones.length; i++){   // t.c.
-        if (jewels.indexOf(stones[i]) !== -1) {
+    for(let i = 0; i < stones.length; i++){   // T.C. for this loop: 0(n)
+        if (jewels.indexOf(stones[i]) !== -1) {   // For each stone, the function calls 'jewels.indexOf(stones[i]')
+            // indexOf method has a T.C of O(m), where m = length of 'jewels'
             countjewels++;
         }
     }
     return countjewels;
+
+    // Overall T.C.: O(n*m), where n = length of 'stones' string and m = length of 'jewels' string
 };
