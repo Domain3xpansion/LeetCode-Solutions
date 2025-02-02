11b62a5ea4af136c8f25d74f62aa29aeb5fc3324
Maikelele <85241561+Domain3xpansion@users.noreply.github.com>
2025-02-02 20:50:06 +0530

    0371-SUM-OF-TWO-INTEGERS Stats: Time: 0 ms (100%), Space: 7.9 MB (2.47%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     int getSum(int a, int b) {
-        int carry;
         while(b != 0){
-            carry = a&b;
-            a = a^b;
-            b = carry<<1;
+            int carry = (a & b) << 1;   //Here, we are calculating a&b first because if we don't do it first, then
+            // we will lose the original value of a because we are storing a^b in a and the original value of a gets 
+            // updated with the newly calculated a^b in each iteration
+            a = a ^ b;   // this gives us the sum of 2 numbers without considering the carry
+            b = carry;
         }
-        return a;
+        return a;   // we return that a^b which gets stored in a itself when carry or a&b becomes 0
     }
 };
