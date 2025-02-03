00cfee24999d80e04310462752a7f2766ae8bbc7
Maikelele <85241561+Domain3xpansion@users.noreply.github.com>
2025-02-04 01:01:45 +0530

    0088-MERGE-SORTED-ARRAY Stats: Time: 0 ms (100%), Space: 12.4 MB (39.81%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,25 +1,18 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        for(int i=m-1; i>=0; --i){
+        for(int i=m-1; i>=0; --i){   // Shifting nums1 elements to the end of the same array
             nums1[i+n] = nums1[i]; 
         }
         int i=n, j=0, k=0; 
         while(k<m+n){
-            if (j >= n || (i < m + n && nums1[i] <= nums2[j])) {   
-                nums1[k++] = nums1[i++];    
-            } else {
-                nums1[k++] = nums2[j++];     
+            if (j>=n || (i<m+n && nums1[i] <= nums2[j])) {  // j>=n becauuse it means all elements in nums2 have 
+            // been processed, and there are no more elements left to merge from nums2 array.  
+                nums1[k++] = nums1[i++];     // Move element from shifted nums1
+            }else{
+                nums1[k++] = nums2[j++];     // Move element from nums2
             }
         }
     }
 };
 
-/*
-
-// j>=n becauuse it means all elements in nums2 have been processed, and there are no more elements left to merge from this array.
-
-// Move element from shifted nums1
-
- // Move element from nums2
- */
