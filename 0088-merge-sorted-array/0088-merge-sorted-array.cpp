d62267b89768390e9dd030d233df3415948db9d0
Maikelele <85241561+Domain3xpansion@users.noreply.github.com>
2025-02-04 23:41:07 +0530

    0088-MERGE-SORTED-ARRAY Stats: Time: 0 ms (100%), Space: 12.2 MB (95.82%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         for(int i=m-1; i>=0; --i){   // Shifting nums1 elements to the end of the same array
-            nums1[i+n] = nums1[i]; 
-        }
+            nums1[i+n] = nums1[i];   // for loop takes 
+        } // Shifting takes O(m)
         int i=n, j=0, k=0; 
         while(k<m+n){
             if (j>=n || (i<m+n && nums1[i] <= nums2[j])) {  // j>=n becauuse it means all elements in nums2 have 
@@ -13,6 +13,10 @@ public:
                 nums1[k++] = nums2[j++];     // Move element from nums2
             }
         }
+        // while loop takes O(m + n)
     }
 };
+
+
+// Total Time Complexity = O(m) + O(m+n) = O(m+n)
 
