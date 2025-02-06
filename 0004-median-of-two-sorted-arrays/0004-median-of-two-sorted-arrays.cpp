5c901d2dddd0b3ed672993a50d50fed70d4dd072
Maikelele <85241561+Domain3xpansion@users.noreply.github.com>
2025-02-06 17:39:33 +0530

    0004-MEDIAN-OF-TWO-SORTED-ARRAYS Stats: Time: 3 ms (33.4%), Space: 96 MB (11.06%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int n1 = nums1.size(), n2 = nums2.size();
+        int n1 = nums1.size(), n2 = nums2.size(), n;
+        n = n1 + n2;
         vector<int> combined;
         int i=0, j=0;
         while(i<n1 && j<n2){
@@ -14,15 +15,17 @@ public:
             combined.push_back(nums1[i++]);
         while(j<n2)
             combined.push_back(nums2[j++]);
-        int n = n1+n2;
         if(n % 2 == 1)
             return (double)combined[n/2];
         else{
             double res = (combined[n/2]+combined[n/2-1]);
-            res = res / 2;
+            res = res/2;
             return res;
         }
     }
 };
 
 
+//Brute force solution
+//T.C = O(n1+n2)
+//S.C = O(n1+n2) because size of the combined array is n1+n2
