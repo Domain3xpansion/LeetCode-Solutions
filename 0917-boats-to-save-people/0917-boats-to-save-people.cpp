1e7191a780dc08b32e59607d84e0c0997a23cfc8
Maikelele <85241561+Domain3xpansion@users.noreply.github.com>
2025-02-12 23:33:43 +0530

    0917-BOATS-TO-SAVE-PEOPLE Stats: Time: 23 ms (20.44%), Space: 45.9 MB (5.28%) - LeetHub

--- 0917-boats-to-save-people/0917-boats-to-save-people.cpp
+++ 0917-boats-to-save-people/0917-boats-to-save-people.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     int numRescueBoats(vector<int>& people, int limit) {
         int i = 0, j = people.size() - 1, count = 0;
-        std::sort(people.begin(), people.end());
-        while(i<=j){
+        std::sort(people.begin(), people.end());      // O(nlogn)
+        while(i<=j){     // Loop runs till O(n)
             if(people[i] + people[j] <= limit){
                 i++;
             }
@@ -14,3 +14,6 @@ public:
     }
 };
 
+// Difficulties faced(if any): coming up with light + heavy approach which will give the minimum, focus on reading constrainsts
+// T.C. =  O(nlogn) + O(n) = O(n log n)
+// S.C. = O(1) 
