73c7328c003cf3869f0a0aac0ba03face795e58a
Maikelele <85241561+Domain3xpansion@users.noreply.github.com>
2025-02-12 23:37:30 +0530

    1988-MINIMIZE-MAXIMUM-PAIR-SUM-IN-ARRAY Stats: Time: 162 ms (87.64%), Space: 99.9 MB (85.81%) - LeetHub

--- 1988-minimize-maximum-pair-sum-in-array/1988-minimize-maximum-pair-sum-in-array.cpp
+++ 1988-minimize-maximum-pair-sum-in-array/1988-minimize-maximum-pair-sum-in-array.cpp
@@ -4,7 +4,7 @@ public:
         int i = 0, j = nums.size() - 1, minimizedmaxsum = INT_MIN;
         std::sort(nums.begin(), nums.end());     
         while(i<j){     
-            if(nums[i] + nums[j] >= minimizedmaxsum){
+            if(nums[i] + nums[j] > minimizedmaxsum){
                 minimizedmaxsum = nums[i] + nums[j];
             }
             i++;
@@ -13,3 +13,8 @@ public:
         return minimizedmaxsum;
     }
 };
+
+
+// Difficulties faced(if any): coming up with light + heavy approach which will give the minimum, focus on reading constrainsts
+// T.C. =  O(nlogn) + O(n/2) = O(n log n)
+// S.C. = O(1) 
