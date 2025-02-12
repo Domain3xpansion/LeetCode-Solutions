d4e280dfb895e7425bc1a573085434adc70bbf23
Maikelele <85241561+Domain3xpansion@users.noreply.github.com>
2025-02-12 23:41:44 +0530

    1988-MINIMIZE-MAXIMUM-PAIR-SUM-IN-ARRAY Stats: Time: 176 ms (44.73%), Space: 100.1 MB (9.23%) - LeetHub

--- 1988-minimize-maximum-pair-sum-in-array/1988-minimize-maximum-pair-sum-in-array.cpp
+++ 1988-minimize-maximum-pair-sum-in-array/1988-minimize-maximum-pair-sum-in-array.cpp
@@ -3,7 +3,7 @@ public:
     int minPairSum(vector<int>& nums) {
         int i = 0, j = nums.size() - 1, minimizedmaxsum = INT_MIN;
         std::sort(nums.begin(), nums.end());     
-        while(i<j){     
+        while(i<j){     // T.C of this loop is O(n/2) which simplifies to O(n)
             if(nums[i] + nums[j] > minimizedmaxsum){
                 minimizedmaxsum = nums[i] + nums[j];
             }
@@ -15,6 +15,6 @@ public:
 };
 
 
-// Difficulties faced(if any): coming up with light + heavy approach which will give the minimum, focus on reading constrainsts
+// Difficulties faced(if any): why even sort the array in the first place ? understanding "The maximum pair sum is minimized"; what is meant by optimally pairing up the elements ?
 // T.C. =  O(nlogn) + O(n/2) = O(n log n)
 // S.C. = O(1) 
