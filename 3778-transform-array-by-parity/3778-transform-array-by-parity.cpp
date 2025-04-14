1ba0bbf2ecb6a0260a5343f6116c1f016082093f
Maikelele <85241561+Domain3xpansion@users.noreply.github.com>
2025-04-14 12:07:17 +0530

    3778-TRANSFORM-ARRAY-BY-PARITY Stats: Time: 0 ms (100%), Space: 28.4 MB (27.74%) - LeetHub

--- 3778-transform-array-by-parity/3778-transform-array-by-parity.cpp
+++ 3778-transform-array-by-parity/3778-transform-array-by-parity.cpp
@@ -1,13 +1,23 @@
 class Solution {
 public:
     vector<int> transformArray(vector<int>& nums) {
+        int i  = 0, j = nums.size() - 1;
         for(auto &val : nums){
             if(val%2==0)
                 val = 0;
             else
                 val = 1;
         }
-        sort(nums.begin(), nums.end());
+        while(i<j){
+            if(nums[i]==1 && nums[j]==0){
+                swap(nums[i], nums[j]);
+                i++;
+                j--;
+            }else if(nums[i]==0)
+               i++;
+            else
+                j--;
+        }
         return nums;
     }
 };
