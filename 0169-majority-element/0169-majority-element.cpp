4fa7f7ed6537eb15e737d73ae9321d227c3ed255
Maikelele <85241561+Domain3xpansion@users.noreply.github.com>
2025-04-19 10:48:31 +0530

    0169-MAJORITY-ELEMENT Stats: Time: 1 ms (37.39%), Space: 28 MB (84.03%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        unordered_map<int, int> hashmap;
+        /*unordered_map<int, int> hashmap;
         for(int x : nums){
             hashmap[x]++;
         }
@@ -9,6 +9,25 @@ public:
             if(pair.second > (nums.size()/2))
                 return pair.first;
         }
+        return -1;*/
+        int cnt = 0;
+        int el;
+        for(int x : nums){
+            if(cnt == 0) {
+                cnt = 1;
+                el = x;
+            }else if(x == el)
+                cnt++;
+            else
+                cnt--;
+        }
+        int cnt1 = 0;
+        for(int x : nums){
+            if(x == el)
+                cnt1++;
+        }
+        if (cnt1 > nums.size() / 2)
+            return el;
         return -1;
     }
 };
