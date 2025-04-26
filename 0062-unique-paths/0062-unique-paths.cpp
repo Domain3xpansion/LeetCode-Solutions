d57b3b6e15ff6b9d4097fbd7a46dce6a74c1b73f
Maikelele <85241561+Domain3xpansion@users.noreply.github.com>
2025-04-26 12:41:12 +0530

    0062-UNIQUE-PATHS Stats: Time: 0 ms (100%), Space: 7.9 MB (91.62%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,20 +1,27 @@
 class Solution {
 public:
-    /*int uniquePathsactual(int i, int j, int m, int n){
-        if(i>=n || j>=m) return 0;
-        if(i==(n-1) && j==(m-1)) return 1;
+    /*
+    int uniquePathsactual(int i, int j, int m, int n){
+        if(i>=m || j>=n) return 0;
+        if(i==(m-1) && j==(n-1)) return 1;
         return uniquePathsactual(i+1, j, m, n) + uniquePathsactual(i, j+1, m, n);
     } 
-    */
+    /*
     int uniquePathsactual(int i, int j, int m, int n, vector<vector<int>> &dp){
         if(i>=m || j>=n) return 0;
         if(i==(m-1) && j==(n-1)) return 1;
         if(dp[i][j]!=-1) return dp[i][j];
         else return dp[i][j] = uniquePathsactual(i+1, j, m, n, dp) + uniquePathsactual(i, j+1, m, n, dp);
     }
+    */
     int uniquePaths(int m, int n){
-        vector<vector<int>> dp(m, vector<int>(n, -1));
-        return uniquePathsactual(0, 0, m, n, dp);
+        //vector<vector<int>> dp(m, vector<int>(n, -1));
+        //return uniquePathsactual(0, 0, m, n, dp);
+        int N = m+n-2, R=n-1;
+        double sum = 1;
+        for(int i=1; i<=R; i++){
+            sum = sum * (N-R+i)/i;
+        }
+        return sum;
     }
-    
 };
