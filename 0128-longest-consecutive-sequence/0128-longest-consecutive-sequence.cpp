a7b504323964e0a87ff1ec13a6f0d0f24db5ee7c
Maikelele <85241561+Domain3xpansion@users.noreply.github.com>
2025-04-27 09:33:27 +0530

    0128-LONGEST-CONSECUTIVE-SEQUENCE Stats: Time: 92 ms (31.37%), Space: 88.9 MB (27.66%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -15,6 +15,7 @@ public:
         }
         return lConsec;
         */
+        /*
         int small = INT_MIN, lConsec = 0, consec=0;
         sort(nums.begin(), nums.end());
         for(int i=0; i<nums.size(); i++){
@@ -26,6 +27,22 @@ public:
                 small = nums[i];
             }
             lConsec = max(lConsec, consec);
+        }
+        return lConsec;
+        */
+        if(nums.empty()) return 0;
+        int n = nums.size(), consec, lConsec=0;
+        unordered_set<int> us(nums.begin(), nums.end());
+        for(auto num : us){
+            if(us.find(num-1) == us.end()){
+                consec = 1;
+                int x = num;
+                while(us.find(x+1) != us.end()){
+                    consec++;
+                    x++;
+                }
+                lConsec = max(lConsec, consec);
+            } 
         }
         return lConsec;
     }
