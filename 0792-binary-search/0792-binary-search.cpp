e5fa64bfd8ac2185513ac63e2f905b535cdbddbd
Maikelele <85241561+Domain3xpansion@users.noreply.github.com>
2025-04-29 22:25:05 +0530

    0792-BINARY-SEARCH Stats: Time: 0 ms (100%), Space: 31.2 MB (80.33%) - LeetHub

--- 0792-binary-search/0792-binary-search.cpp
+++ 0792-binary-search/0792-binary-search.cpp
@@ -1,7 +1,18 @@
 class Solution {
 public:
+    int binarysearch(vector<int> &arr, int target, int s, int e){
+        if(s>e)
+            return -1;
+        int mid = s+(e-s)/2;
+        if(arr[mid]==target)
+            return mid;
+        if(target>arr[mid])
+            return binarysearch(arr, target, mid+1, e);
+        return binarysearch(arr, target, s, mid-1);
+    }
+
     int search(vector<int>& nums, int target) {
-        int s = 0, e = nums.size()-1;
+        /*int s = 0, e = nums.size()-1;
         while(s <= e){
             int mid = s + (e - s) / 2;
             if(target == nums[mid])
@@ -12,5 +23,7 @@ public:
                 e = mid - 1;
         }
         return -1;
+        */
+        return binarysearch(nums, target, 0, nums.size()-1);
     }
 };
