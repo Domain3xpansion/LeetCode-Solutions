a2cba915193bece23a8282800df1fa459b9e944f
Maikelele <85241561+Domain3xpansion@users.noreply.github.com>
2025-04-30 23:12:53 +0530

    0412-FIZZ-BUZZ Stats: Time: 0 ms (100%), Space: 11.8 MB (44.61%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.cpp
+++ 0412-fizz-buzz/0412-fizz-buzz.cpp
@@ -3,6 +3,19 @@ public:
     vector<string> fizzBuzz(int n) {
         vector<string> arr;
         for(int i=1; i<=n; i++){
+            string s = "";
+            if(i%3 == 0)
+                s.append("Fizz");
+            if(i%5 == 0)
+                s.append("Buzz");
+            if(s.empty())
+                s.append(to_string(i));
+            arr.push_back(s);
+        }
+        return arr;
+        /*
+        vector<string> arr;
+        for(int i=1; i<=n; i++){
             if(i%15 == 0)
                 arr.push_back("FizzBuzz");
             else if(i%3 == 0)
@@ -15,5 +28,6 @@ public:
             }
         }
         return arr;
+        */
     }
 };
