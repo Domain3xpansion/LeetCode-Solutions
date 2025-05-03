3c143ba6faa57841642a04faced0d732fb58f658
Maikelele <85241561+Domain3xpansion@users.noreply.github.com>
2025-05-03 10:09:26 +0530

    0078-SUBSETS Stats: Time: 271 ms (6.16%), Space: 105.9 MB (6.99%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,7 +1,20 @@
 class Solution {
 public:
+    vector<vector<int>> helper(vector<int> vec, vector<int> current, vector<vector<int>>& result){
+        if(vec.empty()){
+            result.push_back(current);
+            return result;
+        }
+        int first = vec[0];
+        vector<int> rest(vec.begin() + 1, vec.end());
+        helper(rest, current, result);
+        current.push_back(first);
+        helper(rest, current, result);
+        return result;
+    }
+
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> outer = {{}};
+        /*vector<vector<int>> outer = {{}};
         for(int num : nums){
             int n = outer.size();
             for(int i = 0; i < n; ++i){
@@ -11,5 +24,9 @@ public:
             }
         }
         return outer;
+        */
+        vector<vector<int>> result;
+        vector<int> current;
+        return helper(nums, current, result);
     }
 };
