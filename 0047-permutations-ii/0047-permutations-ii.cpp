c5287ccf0c178998d685fa0f9dd579aa86ec1109
Maikelele <85241561+Domain3xpansion@users.noreply.github.com>
2025-05-13 23:42:33 +0530

    0047-PERMUTATIONS-II Stats: Time: 248 ms (5.01%), Space: 74.8 MB (5.24%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
     void permutations(vector<int> vec, vector<int> freq, set<vector<int>> &ans, vector<int> &ds){
-    if(ds.size() == vec.size()){
-        ans.insert(ds);
-        return;
-    }
-    for(int i=0; i<vec.size(); i++){
-        if(!freq[i]){
-            ds.push_back(vec[i]);
-            freq[i] = 1;
-            permutations(vec, freq, ans, ds);
-            freq[i] = 0;
-            ds.pop_back();
+        if(ds.size() == vec.size()){
+            ans.insert(ds);
+            return;
+        }
+        for(int i=0; i<vec.size(); i++){
+            if(!freq[i]){
+                ds.push_back(vec[i]);
+                freq[i] = 1;
+                permutations(vec, freq, ans, ds);
+                freq[i] = 0;
+                ds.pop_back();
+            }
         }
     }
-}
+    
     vector<vector<int>> permuteUnique(vector<int>& nums) {
         vector<int> ds, freq(nums.size(), 0);
         set<vector<int>> ans;
