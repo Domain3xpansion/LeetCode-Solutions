75058144d022cceae2816295c5f0454070d4c31e
Maikelele <85241561+Domain3xpansion@users.noreply.github.com>
2025-05-15 00:23:44 +0530

    0046-PERMUTATIONS Stats: Time: 4 ms (14.72%), Space: 11.9 MB (18.27%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void permutations(vector<int> vec, vector<int> &freq, vector<vector<int>> &ans, vector<int> &ds){
+    /*void permutations(vector<int> vec, vector<int> &freq, vector<vector<int>> &ans, vector<int> &ds){
         if(ds.size() == vec.size()){
             ans.push_back(ds);
             return;
@@ -14,14 +14,32 @@ public:
                 ds.pop_back();
             }
         }
+    }*/
+
+    
+    void permutationsswap(int index, vector<int> vec, vector<vector<int>> &ans2){
+        if(index == vec.size()){
+            ans2.push_back(vec);
+            return;
+        }
+        for(int i=index; i<vec.size(); i++){
+            swap(vec[index], vec[i]);
+            permutationsswap(index+1, vec, ans2);
+            swap(vec[index], vec[i]);       
+        }
     }
     
+    
     vector<vector<int>> permute(vector<int>& nums) {
+        /*
         vector<int> ds;
         vector<vector<int>> ans;
         int n = nums.size();
         vector<int> freq(n, 0);
         permutations(nums, freq, ans, ds);
-        return ans;
+        return ans;*/
+        vector<vector<int>> ans2;
+        permutationsswap(0, nums, ans2);
+        return ans2;
     }
 };
