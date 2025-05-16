7fbd1b6b324a4e6d0503462184c621f1eb768f42
Maikelele <85241561+Domain3xpansion@users.noreply.github.com>
2025-05-16 12:08:49 +0530

    0047-PERMUTATIONS-II Stats: Time: 6 ms (42.8%), Space: 16.1 MB (12.79%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     //Approach 1 - using the frequency of all numbers present in the given vector(using unordered_map)
-    void permuteUniquehelper(vector<int>& ds, unordered_map<int, int> &um, vector<vector<int>> &result, int n){
+    /*void permuteUniquehelper(vector<int>& ds, unordered_map<int, int> &um, vector<vector<int>> &result, int n){
         if(ds.size() == n) { 
             result.push_back(ds);   //O(n) time
             return;
@@ -17,9 +17,28 @@ public:
             ds.pop_back();
             um[num]++;
         }
+    }*/
+
+    void permutations2swap(int index, vector<int> vec, vector<vector<int>> &ans2){
+    if(index == vec.size()){
+        ans2.push_back(vec);
+        return;
     }
+    unordered_set<int> us;
+    for(int i=index; i<vec.size(); i++){ 
+        if(us.find(vec[i]) != us.end()) continue;
+
+        us.insert(vec[i]);
+        swap(vec[index], vec[i]);
+
+        permutations2swap(index+1, vec, ans2);
+
+        swap(vec[index], vec[i]);        
+    }
+}
     
     vector<vector<int>> permuteUnique(vector<int>& nums){
+        /*
         int n = nums.size();
         vector<vector<int>> result;
         vector<int> temp;   // will store the each permutation
@@ -28,6 +47,9 @@ public:
             um[num]++;
         }
         permuteUniquehelper(temp, um, result, n);
-        return result;
+        return result;*/
+        vector<vector<int>> ans2;
+        permutations2swap(0, nums, ans2);
+        return ans2;
     }
 };
