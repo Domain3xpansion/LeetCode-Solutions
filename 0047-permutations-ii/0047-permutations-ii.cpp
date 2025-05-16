168fba663491a19823015734763217bc0e305342
Maikelele <85241561+Domain3xpansion@users.noreply.github.com>
2025-05-16 11:27:10 +0530

    0047-PERMUTATIONS-II Stats: Time: 0 ms (100%), Space: 11.6 MB (71.36%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
+    //Approach 1 - using the frequency of all numbers present in the given vector(using unordered_map)
     void permuteUniquehelper(vector<int>& ds, unordered_map<int, int> &um, vector<vector<int>> &result, int n){
         if(ds.size() == n) { 
-            result.push_back(ds);
+            result.push_back(ds);   //O(n) time
             return;
         }
         for(auto& [num, count] : um){
@@ -10,7 +11,9 @@ public:
                 continue;
             ds.push_back(num);
             um[num]--;
+
             permuteUniquehelper(ds, um, result, n);
+
             ds.pop_back();
             um[num]++;
         }
@@ -18,12 +21,12 @@ public:
     
     vector<vector<int>> permuteUnique(vector<int>& nums){
         int n = nums.size();
-        unordered_map<int, int> um;
+        vector<vector<int>> result;
+        vector<int> temp;   // will store the each permutation
+        unordered_map<int, int> um;    // to store frequency of every number present in nums vector
         for(auto& num : nums){     // count the occurrences of each number
             um[num]++;
         }
-        vector<int> temp;
-        vector<vector<int>> result;
         permuteUniquehelper(temp, um, result, n);
         return result;
     }
