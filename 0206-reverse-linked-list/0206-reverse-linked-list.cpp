97040625e48f6897b729dd9d140aa81b0c54cb04
Maikelele <85241561+Domain3xpansion@users.noreply.github.com>
2025-05-26 11:37:34 +0530

    0206-REVERSE-LINKED-LIST Stats: Time: 0 ms (100%), Space: 13.4 MB (39.54%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -12,11 +12,22 @@ class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
         // It is a recursive solution
-        if(head == NULL || head->next == NULL) return head;      
+        /*if(head == NULL || head->next == NULL) return head;      
         ListNode* newhead = reverseList(head->next);
         ListNode* front = head->next;      
         front->next = head;            
         head->next = NULL;             
-        return newhead;
+        return newhead;*/
+        // It is a iterative solution
+        if(head == NULL || head->next == NULL) return head;
+        ListNode* temp = head;
+        ListNode* prev = NULL;
+        while(temp != NULL){
+            ListNode* nexxt = temp->next;
+            temp->next = prev;
+            prev = temp;
+            temp = nexxt;
+        }
+        return prev;
     }
 };
