49aeeece81ab11e5daf873709effc91927f94c7e
Maikelele <85241561+Domain3xpansion@users.noreply.github.com>
2025-05-27 22:37:05 +0530

    0141-LINKED-LIST-CYCLE Stats: Time: 8 ms (81.28%), Space: 11.9 MB (23.04%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -10,7 +10,7 @@ class Solution {
 public:
     bool hasCycle(ListNode *head) {
         //Naive solution
-        unordered_map<ListNode*, int> um;
+        /*unordered_map<ListNode*, int> um;
         ListNode* temp = head;
         while(temp != NULL){
             auto it = um.find(temp);
@@ -19,6 +19,16 @@ public:
             }
             um[temp] = 1;
             temp = temp->next;
+        }
+        return false;*/
+        // Tortoise and Hare Algo
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast != NULL && fast->next != NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow == fast)
+                return true;
         }
         return false;
     }
