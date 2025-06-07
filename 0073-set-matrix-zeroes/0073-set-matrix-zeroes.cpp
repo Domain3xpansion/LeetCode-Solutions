ae1f547da90ba2f41deac5b4fa30cb286a5279e1
Maikelele <85241561+Domain3xpansion@users.noreply.github.com>
2025-06-07 17:43:32 +0530

    0073-SET-MATRIX-ZEROES Stats: Time: 0 ms (100%), Space: 18.7 MB (50.89%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -41,6 +41,7 @@ public:
         }
     }*/
 
+    /* Better solution
     void setZeroes(vector<vector<int>>& matrix) {  
         int m = matrix.size();     //stores no of rows
         int n = matrix[0].size();     //stores no of columns
@@ -64,5 +65,41 @@ public:
         //This above nested for loop is of O(m*n)
         //Total time complexity = O(2*m*n) 
         //Space complexity = O(m) + O(n) (for the 2 vectors that we are using for hashing)
+    }*/
+
+    // Optimal solution
+    void setZeroes(vector<vector<int>>& matrix) {  
+        int col0 = 1;
+        int m = matrix.size();     
+        int n = matrix[0].size();
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(matrix[i][j] == 0){
+                    matrix[i][0] = 0;
+                    if(j != 0)
+                        matrix[0][j] = 0;
+                    else
+                        col0 = 0;
+                }
+            }
+        }
+        for(int i=1; i<m; i++){
+            for(int j=1; j<n; j++){
+                if(matrix[i][j] != 0){
+                    if(matrix[0][j] == 0 || matrix[i][0] == 0)
+                        matrix[i][j] = 0;
+                }
+            }
+        }
+        if(matrix[0][0] == 0){
+            for(int j=0; j<n; j++){
+                matrix[0][j] = 0;
+            }
+        }
+        if(col0 == 0){
+            for(int i=0; i<m; i++){
+                matrix[i][0] = 0;
+            }
+        }
     }
 };
