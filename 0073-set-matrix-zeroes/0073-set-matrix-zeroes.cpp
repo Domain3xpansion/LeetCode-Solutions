62942ba9c53fe5973296acd1996fecd316e32533
Maikelele <85241561+Domain3xpansion@users.noreply.github.com>
2025-06-07 17:13:29 +0530

    0073-SET-MATRIX-ZEROES Stats: Time: 0 ms (100%), Space: 18.8 MB (13.23%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -5,7 +5,7 @@ public:
     
     for the 1st nested for loops: O(m Ã n) to scan every element
     for each zero found: O(m + n) to mark entire row and column
-    for the 2nd nested for loops: O(m Ã n) where we are replacing every 'a' with 0*/
+    for the 2nd nested for loops: O(m Ã n) where we are replacing every 'a' with 0
 
     void markrows(vector<vector<int>>& matrix, int i){
         int n = matrix[0].size();
@@ -35,9 +35,34 @@ public:
         }
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
-                if (matrix[i][j] == 'a')
+                if(matrix[i][j] == 'a')
                     matrix[i][j] = 0;
             }
         }
+    }*/
+
+    void setZeroes(vector<vector<int>>& matrix) {  
+        int m = matrix.size();     //stores no of rows
+        int n = matrix[0].size();     //stores no of columns
+        vector<int> rows(m, 0);    //responsible for marking cells with 1 for which columns have a min of one 0
+        vector<int> columns(n, 0);   //responsible for marking cells with 1 for which rows have a min of one 0
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(matrix[i][j] == 0){
+                    rows[i] = 1;
+                    columns[j] = 1;
+                }
+            }
+        }
+        //This above  O(m*n)
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){  // if either of rows[i] or columns[j] has 1 in it, make the cell of "matrix" at [i][j] change to 0
+                if(rows[i] || columns[j])
+                    matrix[i][j] = 0;
+            }
+        }
+        //This above nested for loop is of O(m*n)
+        //Total time complexity = O(2*m*n) 
+        //Space complexity = O(m) + O(n) (for the 2 vectors that we are using for hashing)
     }
 };
