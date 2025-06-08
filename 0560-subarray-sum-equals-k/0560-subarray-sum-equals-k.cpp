485787384f1fbd2e76215a70b27ba0e54af8cf93
Maikelele <85241561+Domain3xpansion@users.noreply.github.com>
2025-06-08 08:33:51 +0530

    0560-SUBARRAY-SUM-EQUALS-K Stats: Time: 55 ms (46.71%), Space: 51.1 MB (13.12%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,13 +1,23 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        int n = nums.size(), sum, count=0;
+        /*int n = nums.size(), sum, count=0;
         for(int i=0; i<n; i++){
             sum = 0;
             for(int j=i; j<n; j++){
                 sum += nums[j];
                 if(k == sum) count++; 
             }
+        }
+        return count;*/
+        unordered_map<int, int> um;
+        um[0] = 1;
+        int presum = 0, count = 0;
+        for(int i=0; i<nums.size(); i++){
+            presum += nums[i];
+            int remove = presum - k;
+            count += um[remove];
+            um[presum] += 1;
         }
         return count;
     }
