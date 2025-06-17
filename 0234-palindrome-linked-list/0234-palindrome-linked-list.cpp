75562425e2f0efa4d83f8917f103625b7d0e6340
Maikelele <85241561+Domain3xpansion@users.noreply.github.com>
2025-06-17 23:26:00 +0530

    0234-PALINDROME-LINKED-LIST Stats: Time: 0 ms (100%), Space: 121.8 MB (49.92%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -10,8 +10,20 @@
  */
 class Solution {
 public:
+    ListNode* reverseList(ListNode* head){
+        if(head == NULL || head->next == NULL) return head;
+        ListNode* temp = head;
+        ListNode* prev = NULL;
+        while(temp != NULL){
+            ListNode* nexxt = temp->next;
+            temp->next = prev;
+            prev = temp;
+            temp = nexxt;
+        }
+        return prev;
+    }
     bool isPalindrome(ListNode* head) {
-        ListNode* temp = head;                       
+        /*ListNode* temp = head;                       
         stack<int> s;                            
         while(temp != NULL){
             s.push(temp->val);
@@ -24,6 +36,27 @@ public:
             temp = temp->next;
             s.pop();
         }       
+        return true;*/
+        if(head == NULL || head->next == NULL)     
+            return true;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast->next != NULL && fast->next->next != NULL){             
+            slow = slow->next;                              
+            fast = fast->next->next;
+        }
+        ListNode* newhead = reverseList(slow->next);         
+        ListNode* first = head;
+        ListNode* second = newhead;
+        while(second != NULL){           
+            if(first->val != second->val){
+                reverseList(newhead);      
+                return false;
+            }
+            first = first->next;
+            second = second->next;
+        }
+        reverseList(newhead);           
         return true;
     }
 };
