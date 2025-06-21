46ca5d244ec6834b6223eb679540f81a9cb4405e
Maikelele <85241561+Domain3xpansion@users.noreply.github.com>
2025-06-21 10:59:57 +0530

    0874-BACKSPACE-STRING-COMPARE Stats: Time: 0 ms (100%), Space: 8.4 MB (85.89%) - LeetHub

--- 0874-backspace-string-compare/0874-backspace-string-compare.cpp
+++ 0874-backspace-string-compare/0874-backspace-string-compare.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool backspaceCompare(string s, string t) {
-        int n = s.size(), m = t.size(), i=0;
+        /*int n = s.size(), m = t.size(), i=0;
         string res1 = "", res2 = "";
         while(i<n){
             if(s[i]!='#')
@@ -18,6 +18,40 @@ public:
                 res2.pop_back();
             i++;
         }
-        return res1 == res2;
+        return res1 == res2;*/
+
+        int n = s.size(), m = t.size(), i=n-1, j=m-1;
+        int skips = 0, skipt = 0;
+        while(i >= 0 || j >= 0){
+            while(i >= 0){
+                if(s[i] == '#'){
+                    skips++;
+                    i--;
+                }else if(skips > 0){
+                    skips--;
+                    i--;
+                }else{
+                    break;
+                }
+            }
+            while(j >= 0){
+                if(t[j] == '#'){
+                    skipt++;
+                    j--;
+                }else if(skipt > 0){
+                    skipt--;
+                    j--;
+                }else{
+                    break;
+                }
+            }
+            char si = i < 0 ? '$' : s[i];
+            char tj = j < 0 ? '$' : t[j];
+            if(si != tj)
+                return false;
+            i--;
+            j--;
+        }
+        return true;
     }
 };
