b8c3effcb58c3032462411468763c85910fbf03f
Maikelele <85241561+Domain3xpansion@users.noreply.github.com>
2025-06-25 23:36:10 +0530

    2107-FIND-UNIQUE-BINARY-STRING Stats: Time: 3 ms (23.8%), Space: 12.9 MB (49.1%) - LeetHub

--- 2107-find-unique-binary-string/2107-find-unique-binary-string.cpp
+++ 2107-find-unique-binary-string/2107-find-unique-binary-string.cpp
@@ -1,13 +1,27 @@
 class Solution {
 public:
     string findDifferentBinaryString(vector<string>& nums) {
-        int n = nums.size();
+        /*int n = nums.size();
         string res;
         unordered_set<int> us;
         for(int i=0; i<n; i++){
             us.insert(stoi(nums[i], 0, 2));
         }
         for(int i=0; i<pow(2, n); i++){
+            if(us.find(i) == us.end()){
+                res = bitset<16>(i).to_string();
+                return res.substr(16-n, n);
+            }
+        }
+        return "";*/
+
+        int n = nums.size();
+        string res;
+        unordered_set<int> us;
+        for(int i=0; i<n; i++){
+            us.insert(stoi(nums[i], 0, 2));
+        }
+        for(int i=0; i<=n; i++){
             if(us.find(i) == us.end()){
                 res = bitset<16>(i).to_string();
                 return res.substr(16-n, n);
