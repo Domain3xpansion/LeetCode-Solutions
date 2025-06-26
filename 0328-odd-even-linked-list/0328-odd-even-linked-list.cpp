7c1c489c60fc7f24e4a0d96be0e4f2f7419aa1f2
Maikelele <85241561+Domain3xpansion@users.noreply.github.com>
2025-06-26 11:38:52 +0530

    0328-ODD-EVEN-LINKED-LIST Stats: Time: 0 ms (100%), Space: 15.4 MB (87.69%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
@@ -11,9 +11,8 @@
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
-         if (head == NULL || head->next == NULL) {
+        /*if(head == NULL || head->next == NULL)
             return head;
-        }
         ListNode* temp = head;
         vector<int> vec;
         while(temp != NULL && temp->next != NULL){
@@ -31,11 +30,25 @@ public:
             vec.push_back(temp->val);
         temp = head;
         int i=0;
-        while(temp != NULL && i < vec.size()){
+        while(temp != NULL){
             temp->val = vec[i];
             temp = temp->next;
             i++;
         }
+        return head;*/
+
+        if(head == NULL || head->next == NULL)
+            return head;
+        ListNode* todd = head;
+        ListNode* even = head->next;
+        ListNode* teven = head->next;
+        while(teven != NULL && teven->next != NULL){
+            todd->next = todd->next->next;
+            teven->next = teven->next->next;
+            todd = todd->next;
+            teven = teven->next;
+        }
+        todd->next = even;
         return head;
     }
 };
