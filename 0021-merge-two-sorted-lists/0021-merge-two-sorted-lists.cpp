22ede9aacbdda5f1e8f355d196652d018edaeaa7
Maikelele <85241561+Domain3xpansion@users.noreply.github.com>
2025-06-27 22:50:44 +0530

    0021-MERGE-TWO-SORTED-LISTS Stats: Time: 0 ms (100%), Space: 19.5 MB (29.48%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -22,7 +22,7 @@ public:
         return head;
     }
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode* temp1 = list1;
+        /*ListNode* temp1 = list1;
         ListNode* temp2 = list2;
         vector<int> vec;
         while(temp1 != NULL){
@@ -35,6 +35,24 @@ public:
         }
         sort(vec.begin(), vec.end());
         ListNode* res = convertarrtoll(vec);
-        return res;
+        return res;*/
+        ListNode* dummyNode = new ListNode(-1);
+        ListNode* temp = dummyNode;
+        while(list1 != NULL && list2 != NULL){
+            if(list1->val < list2->val){
+                temp->next = list1;
+                temp = list1;
+                list1 = list1->next;
+            }else{
+                temp->next = list2;
+                temp = list2;
+                list2 = list2->next;
+            }
+        }
+        if(list1) 
+            temp->next = list1;
+        else 
+            temp->next = list2;
+        return dummyNode->next;
     }
 };
