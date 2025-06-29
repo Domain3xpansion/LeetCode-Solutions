4ed21032a3ad2482e1e93c3322d70447bfaf83ed
Maikelele <85241561+Domain3xpansion@users.noreply.github.com>
2025-06-29 12:53:12 +0530

    1469-MINIMUM-NUMBER-OF-STEPS-TO-MAKE-TWO-STRINGS-ANAGRAM Stats: Time: 3 ms (81.42%), Space: 19.3 MB (75.03%) - LeetHub

--- 1469-minimum-number-of-steps-to-make-two-strings-anagram/1469-minimum-number-of-steps-to-make-two-strings-anagram.cpp
+++ 1469-minimum-number-of-steps-to-make-two-strings-anagram/1469-minimum-number-of-steps-to-make-two-strings-anagram.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int minSteps(string s, string t) {
-        int n = s.size(), result = 0;
+        /*int n = s.size(), result = 0;
         int freq_s[26] = {0}, freq_t[26] = {0};
         for(int i=0; i<n; i++){
             freq_s[s[i]-'a']++;
@@ -10,6 +10,17 @@ public:
         for(int i=0; i<26; i++){
             if(freq_s[i]>freq_t[i])
                 result += freq_s[i]-freq_t[i];
+        }
+        return result;*/
+        int n = s.size(), result = 0;
+        int freq[26] = {0};
+        for(int i=0; i<n; i++){
+            freq[s[i]-'a']++;
+            freq[t[i]-'a']--;
+        }
+        for(int i=0; i<26; i++){
+            if(freq[i]>0)
+                result += freq[i];
         }
         return result;
     }
