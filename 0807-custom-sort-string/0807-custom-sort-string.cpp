0e67d026294bd33024291993cf8a41434af6b34c
Maikelele <85241561+Domain3xpansion@users.noreply.github.com>
2025-07-02 00:05:15 +0530

    0807-CUSTOM-SORT-STRING Stats: Time: 0 ms (100%), Space: 8.2 MB (73.6%) - LeetHub

--- 0807-custom-sort-string/0807-custom-sort-string.cpp
+++ 0807-custom-sort-string/0807-custom-sort-string.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     string customSortString(string order, string s) {
-        string result;
+        /*string result;
         unordered_map<char, int> um;
         for(char &i : s){
             um[i]++;
@@ -17,6 +17,17 @@ public:
                 result.push_back(i);
             }
         }
-        return result;
+        return result;*/
+
+        vector<int> index(26, -1);
+        for(int i = 0; i < order.length(); i++){
+            char ch = order[i];
+            index[ch - 'a'] = i;
+        }
+        auto comparator = [&index](char &ch1, char &ch2){
+            return index[ch1-'a'] < index[ch2-'a'];
+        };
+        sort(s.begin(), s.end(), comparator);
+        return s;
     }
 };
