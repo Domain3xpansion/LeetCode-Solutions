377cdb3def4a147152880d1590d08b8307ee3da8
Maikelele <85241561+Domain3xpansion@users.noreply.github.com>
2025-07-03 09:54:55 +0530

    0165-COMPARE-VERSION-NUMBERS Stats: Time: 0 ms (100%), Space: 8.3 MB (69.13%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.cpp
+++ 0165-compare-version-numbers/0165-compare-version-numbers.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    vector<string> tokens(string a){
+    /*vector<string> tokens(string a){
         stringstream ss(a);
         string token = "";
         vector<string> tokens;
@@ -8,9 +8,9 @@ public:
             tokens.push_back(token);
         }
         return tokens;
-    }
+    }*/
     int compareVersion(string version1, string version2){
-        vector<string> vers1 = tokens(version1);
+        /*vector<string> vers1 = tokens(version1);
         vector<string> vers2 = tokens(version2);
         int v1 = vers1.size(), v2 = vers2.size(), i=0;
         while(i < v1 || i < v2){
@@ -22,6 +22,26 @@ public:
                 return 1;
             else
                 i++;
+        }
+        return 0;*/
+
+        int i = 0, j = 0;
+        while(i < version1.length() || j < version2.length()){
+            int num1 = 0, num2 = 0;
+            while(i < version1.length() && version1[i] != '.'){
+                num1 = num1 * 10 + (version1[i] - '0');
+                i++;
+            }
+            i++; 
+            while (j < version2.length() && version2[j] != '.') {
+                num2 = num2 * 10 + (version2[j] - '0');
+                j++;
+            }
+            j++;
+            if(num1 < num2) 
+                return -1;
+            if(num1 > num2) 
+                return 1;
         }
         return 0;
     }
