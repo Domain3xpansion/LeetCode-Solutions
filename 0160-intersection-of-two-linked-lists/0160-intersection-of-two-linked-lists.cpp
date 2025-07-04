bd48782d8f1f2cc29878d9159e892e7dc5066b72
Maikelele <85241561+Domain3xpansion@users.noreply.github.com>
2025-07-04 22:56:35 +0530

    0160-INTERSECTION-OF-TWO-LINKED-LISTS Stats: Time: 43 ms (24.95%), Space: 18.5 MB (52.86%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -7,7 +7,7 @@
  * };
  */
 class Solution {
-public:
+public:/*
     ListNode *Intersecthelper(ListNode* small, ListNode* large, int diff){
         while(diff){                     // making the pointer of the larger ll cover some ground so that we can start
             large = large->next;         // comparing the nodes from an equal footing
@@ -18,11 +18,11 @@ public:
             large = large->next;
         }
         return small;
-    }
-    // T.C = O(nA) + O(nB) + O(nB-nA) + O(nA) = O(nA + 2nB)
-    // S.C = O(1)
-    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        ListNode* tempA = headA;
+    }*/
+
+    // T.C = O(nA) + O(nB) + O(nB-nA) + O(nA) = O(nA + 2nB)       S.C = O(1)
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB){
+        /*ListNode* tempA = headA;
         ListNode* tempB = headB;
         int nA = 0, nB = 0;
         while(tempA != NULL){
@@ -36,6 +36,20 @@ public:
         if(nA<nB)
             return Intersecthelper(headA, headB, nB-nA);   // 1st param pointer which has travelled less no of nodes and 
         else                                                   // 2nd param pointer which has travelled more
-            return Intersecthelper(headB, headA, nA-nB);
+            return Intersecthelper(headB, headA, nA-nB);*/
+    
+        if(headA == NULL || headB == NULL)     // T.C = O(nA + nB)(because both tempA and tempB are traversing the same no of nodes i.e. nA+nB)      
+            return NULL;                         // S.C = O(1)
+        ListNode* tempA = headA;
+        ListNode* tempB = headB;
+        while(tempA != tempB){   // what if there is only a SLL and both the heads are pointing to the first node of the SLL
+            tempA = tempA->next;
+            tempB = tempB->next;
+            if(tempA == tempB)  // this if check will be true if both the pointers collide or don't collide at all(because both of them will be pointing to NULL)
+                return tempA;
+            if(tempA == NULL) tempA = headB;
+            if(tempB == NULL) tempB = headA;
+        }
+        return tempA;
     }
 };
