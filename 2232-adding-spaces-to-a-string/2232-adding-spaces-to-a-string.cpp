56b9235c82a090c073ac9d9b253ef95c23feeac3
Maikelele <85241561+Domain3xpansion@users.noreply.github.com>
2025-07-12 22:47:53 +0530

    2232-ADDING-SPACES-TO-A-STRING Stats: Time: 19 ms (60.72%), Space: 85.7 MB (32.27%) - LeetHub

--- 2232-adding-spaces-to-a-string/2232-adding-spaces-to-a-string.cpp
+++ 2232-adding-spaces-to-a-string/2232-adding-spaces-to-a-string.cpp
@@ -1,13 +1,24 @@
 class Solution {
 public:
     string addSpaces(string s, vector<int>& spaces) {
-        unordered_set<int> spacePositions(spaces.begin(), spaces.end());
+        /*unordered_set<int> spacePositions(spaces.begin(), spaces.end());
         string result;
         for(int i = 0; i < s.size(); i++){
             if(spacePositions.find(i) != spacePositions.end())
                 result.push_back(' ');
             result.push_back(s[i]);
         }
+        return result;*/
+        string result;
+        int j=0;
+        for(int i = 0; i < s.size(); i++){
+            if(j<spaces.size() && i == spaces[j]){
+                result.push_back(' ');
+                j++;
+            }
+            result.push_back(s[i]);
+        }
         return result;
+
     }
 };
