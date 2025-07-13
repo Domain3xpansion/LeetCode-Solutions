faf0abb9b8bab19f2341812e6b957ab82fcd58ea
Maikelele <85241561+Domain3xpansion@users.noreply.github.com>
2025-07-13 18:29:37 +0530

    0670-MAXIMUM-SWAP Stats: Time: 0 ms (100%), Space: 8 MB (32.49%) - LeetHub

--- 0670-maximum-swap/0670-maximum-swap.cpp
+++ 0670-maximum-swap/0670-maximum-swap.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int maximumSwap(int num) {
-        string str_equiv = to_string(num);
+        /*string str_equiv = to_string(num);
         int n = str_equiv.size();
         vector<int> max_right(n, 0);
         max_right[n-1] = n-1;
@@ -16,6 +16,25 @@ public:
             if(str_equiv[i]<elem){
                 swap(str_equiv[i], str_equiv[ind]);
                 return stoi(str_equiv);
+            }
+        }
+        return num;*/
+
+        vector<int> vec(10, -1);
+        string str_equiv = to_string(num);
+        int n = str_equiv.size();
+        for(int i=0; i<n; i++){
+            int k = str_equiv[i] - '0';
+            vec[k] = i;
+        }
+        for(int i=0; i<n; i++){
+            char ch = str_equiv[i];
+            int dig = ch - '0';
+            for(int j=9; j>dig; j--){
+                if(vec[j]>i){
+                    swap(str_equiv[i], str_equiv[vec[j]]);
+                    return stoi(str_equiv);
+                }
             }
         }
         return num;
