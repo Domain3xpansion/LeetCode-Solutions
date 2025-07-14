78511b3cd28005ca12192e5c07b665c0dcca981e
Maikelele <85241561+Domain3xpansion@users.noreply.github.com>
2025-07-15 00:16:14 +0530

    0018-4SUM Stats: Time: 18 ms (84.3%), Space: 17.6 MB (29.96%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        int n = nums.size();
+        /*int n = nums.size();
         set<vector<int>> unique_quad;
         for(int i=0; i<n; i++){
             for(int j=i+1; j<n; j++){
@@ -20,6 +20,40 @@ public:
             }
         }
         vector<vector<int>> res(unique_quad.begin(), unique_quad.end());
+        return res;*/
+
+        int n = nums.size();
+        vector<vector<int>> res;
+        sort(nums.begin(), nums.end());         // O(n log n)
+        for(int i=0; i<n; i++){
+            if(i>0 && nums[i] == nums[i-1])
+                continue;
+            for(int j=i+1; j<n; j++){
+                if(j>i+1 && nums[j] == nums[j-1])
+                    continue;
+                int k = j+1;
+                int l = n-1;
+                while(k<l){
+                    long long sum = nums[i] + nums[j];
+                    sum += nums[k];
+                    sum += nums[l];
+                    if(sum<target)
+                        k++;
+                    else if(sum>target)
+                        l--;
+                    else{
+                        vector<int> vec = {nums[i], nums[j], nums[k], nums[l]};
+                        res.push_back(vec);
+                        k++;
+                        l--;
+                        while(k<l && nums[k] == nums[k-1])
+                            k++;
+                        while(k<l && nums[l] == nums[l+1])
+                            l--;
+                    }
+                }
+            }
+        }
         return res;
     }
 };
