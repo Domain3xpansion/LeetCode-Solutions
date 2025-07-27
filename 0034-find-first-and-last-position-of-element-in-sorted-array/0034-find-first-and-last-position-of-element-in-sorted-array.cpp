7679fa7ffaea1ce61edc21bf2e903fad09334ee2
Maikelele <85241561+Domain3xpansion@users.noreply.github.com>
2025-07-27 18:48:44 +0530

    0034-FIND-FIRST-AND-LAST-POSITION-OF-ELEMENT-IN-SORTED-ARRAY Stats: Time: 0 ms (100%), Space: 17.4 MB (86.59%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+/*
     int lowerbound(vector<int>& nums, int target){
         int low = 0, high = nums.size() - 1;
         int ans = nums.size();
@@ -35,5 +36,47 @@ public:
         if(lb == nums.size() || nums[lb] != target)
             return {-1, -1};
         return {lb, upperbound(nums, target) - 1};
+    }
+    */
+
+    int firstocc(vector<int>& nums, int target){
+        int low = 0, high = nums.size() - 1;
+        int ans = -1;
+        while(low <= high){
+            long long mid = low + (high - low)/2;
+            if(nums[mid] == target){
+                ans = mid;
+                high = mid - 1;
+            }
+            else if(nums[mid] < target)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+        return ans;
+    }
+
+    int lastocc(vector<int>& nums, int target){
+        int low = 0, high = nums.size() - 1;
+        int ans = -1;
+        while(low <= high){
+            long long mid = low + (high - low)/2;
+            if(nums[mid] == target){
+                ans = mid;
+                low = mid + 1;
+            }
+            else if(nums[mid] < target)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+        return ans;
+    }
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int first = firstocc(nums, target);
+        if(first == -1)
+            return {-1, -1};
+        int last = lastocc(nums, target);
+        return {first, last};
     }
 };
