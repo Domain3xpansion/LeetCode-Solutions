98a896fe36031ccdf12cacb24ef2e05e22757c79
Maikelele <85241561+Domain3xpansion@users.noreply.github.com>
2026-08-23 19:38:40 +0530

    0205-ISOMORPHIC-STRINGS Stats: Time: 3 ms (40.68%), Space: 9.1 MB (95.01%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -23,15 +23,25 @@ public:
         for(int i = 0; i < s.size(); i++){
             char c1 = s[i];
             char c2 = t[i]; 
-            if(!onetoone.count(c1)){
+            if(!onetoone.count(c1)){  // checking if c1 already exists or not
+            // will enter in the if block only if c1 doesn't exist in the map
                 for(auto& [key, val]:onetoone){
                     if(val == c2)
                         return false;
+                        /*checking if c2, which will be the value in our map, is it already mapped to any key present in the map, if yes, then return false
+                        if not, then insert both c1 and c2 where c2 will be c1's value*/
                 }
                 onetoone.insert({c1, c2});
             }
             else{
+                /*will come to the else part only if a key already exists
+                that means that existing key will have a value mapped to it*/
                 if(onetoone[c1] != c2)
+                /* if that value which is mapped to the existing is not equal to the current value, then return false
+                example: in foo -> bar,
+                    the 2nd o will bring the control to this else block here
+                    f -> b, o -> a till now
+                    a != r so return false because "No two characters may map to the same character" */ 
                     return false;
             }
         }
