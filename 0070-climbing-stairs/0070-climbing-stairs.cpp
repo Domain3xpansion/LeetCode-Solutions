b81b0b859c446344b7c1d917ccc7ee77e581d20a
Maikelele <85241561+Domain3xpansion@users.noreply.github.com>
2026-08-29 12:55:31 +0530

    0070-CLIMBING-STAIRS Stats: Time: 0 ms (100%), Space: 7.9 MB (74.7%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -2,6 +2,9 @@ class Solution {
 public:
     int climbStairs(int n) {
         // using Top Down approach
+        /* Two learnings while implementing Top Down approach:
+            1. use static. Because, memoization is not actually being preserved between recursive calls. If static keyword is not used, arr[n] is always 0 when a new function call starts, causing exponential recursion â TLE.
+            2. declared arr of 45 cells, should be 46 */
         static int arr[46] = {0};
         if (n <= 2)
             return n;
