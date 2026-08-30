946331e4f00e13458bd3b3f6dcc5b5066b5baa0f
Maikelele <85241561+Domain3xpansion@users.noreply.github.com>
2026-08-30 13:10:35 +0530

    0747-MIN-COST-CLIMBING-STAIRS Stats: Time: 0 ms (100%), Space: 17.6 MB (75.66%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
@@ -7,6 +7,11 @@ public:
        for(int i = n-3; i >= 0; i--){
             cost[i] = min(cost[i] + cost[i+1], cost[i] + cost[i+2]);
        }
+       /*
+       cost[i] + cost[i+1] = Pay for step i, then take 1 step from i and pay it's cost too. cost[i+1] represents the minimum cost from step i+1 all the way to the top, not merely the original price of that step.
+
+       cost[i] + cost[i+2] = Pay the cost of step i, then take 2 steps, and from there follow the cheapest possible path to the top.
+       */
        return min(cost[0], cost[1]);
     }
 };
