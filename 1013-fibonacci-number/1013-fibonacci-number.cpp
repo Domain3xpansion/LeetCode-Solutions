ff5e3b2cef0dcc7c29a9093c0caf4331cde33c3c
Maikelele <85241561+Domain3xpansion@users.noreply.github.com>
2026-09-01 23:45:33 +0530

    1013-FIBONACCI-NUMBER Stats: Time: 3 ms (64.23%), Space: 8.2 MB (11.47%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,7 +1,21 @@
 class Solution {
 public:
-    int fib(int n) {
+    int solve(int n, vector<int>& dp){ 
+        if(n <= 1) 
+            return n; 
+        if(dp[n] != 0) 
+            return dp[n]; 
+        dp[n] = solve(n - 1, dp) + solve(n - 2, dp); 
+        return dp[n]; 
+    }
+    
+    int fib(int n){
+        //Top down approach
+        vector<int> dp(n+1, 0); 
+        return solve(n, dp);
+
         // Bottom up approach
+        /*
         if(n==0)
             return 0;    
         vector<int> dp(n+1, 0);
@@ -10,6 +24,6 @@ public:
         for(int i=2; i<=n; i++){
             dp[i] = dp[i-1] + dp[i-2];
         }
-        return dp[n];
+        return dp[n];*/
     }
 };
