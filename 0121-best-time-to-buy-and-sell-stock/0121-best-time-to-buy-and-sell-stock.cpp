a3e3c7b2809df0af74803dee792ff4ec0e905ecf
Maikelele <85241561+Domain3xpansion@users.noreply.github.com>
2026-09-07 22:28:19 +0530

    0121-BEST-TIME-TO-BUY-AND-SELL-STOCK Stats: Time: 0 ms (100%), Space: 97.3 MB (61.52%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,13 +1,34 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int profit = 0, minimum = prices[0], max_profit = 0;
+        int profit = 0, cost = 0, mincost = prices[0];
+        for(int i=1; i<=prices.size()-1; i++){
+            mincost = min(mincost, prices[i]);
+            cost = prices[i] - mincost;
+            profit = max(profit, cost);
+        }
+        return profit;
+    }
+};
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        /*int profit = 0, minimum = prices[0], max_profit = 0;
         for(int i=1; i<prices.size(); i++){   //starting from 1 because cant sell the stock on the same day
             profit = prices[i] - minimum;
             minimum = min(minimum, prices[i]);
             if(profit > max_profit)
                 max_profit = profit;
         }
-        return max_profit;
-    }
-};
+        return max_profit;*/
+    /*}
+};*/
