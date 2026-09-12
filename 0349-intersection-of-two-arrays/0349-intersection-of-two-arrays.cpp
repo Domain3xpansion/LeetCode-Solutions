9921dfa322bfb31326e7a839f1018a4a24317ab8
Maikelele <85241561+Domain3xpansion@users.noreply.github.com>
2026-09-12 18:38:24 +0530

    0349-INTERSECTION-OF-TWO-ARRAYS Stats: Time: 3 ms (38.25%), Space: 13.8 MB (91.13%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -1,7 +1,33 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> intersect;
+        // Optimal
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        vector<int> res;
+        int i = 0, j = 0;
+        while(i<nums1.size() && j<nums2.size()){
+            if(nums1[i] < nums2[j])
+            {
+                i++;
+            }
+            else if(nums2[j] < nums1[i])
+            {
+                j++;
+            }
+            else
+            {
+                if(res.empty() || res.back() != nums1[i]){
+                    res.push_back(nums1[i]);
+                }
+                i++;
+                j++;
+            }
+        }
+        return res;
+        
+        // Brute force approach
+        /*vector<int> intersect;
         for(int i=0; i<nums1.size(); i++){
             auto it = find(intersect.begin(), intersect.end(), nums1[i]);
             for(int j=0; j<nums2.size(); j++){
@@ -10,6 +36,6 @@ public:
                 }
             }
         }
-        return intersect;
+        return intersect;*/
     }
 };
