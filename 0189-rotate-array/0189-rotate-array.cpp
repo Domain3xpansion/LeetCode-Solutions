934d4300ac14247ae3c5d43cf656c7db3de38959
Maikelele <85241561+Domain3xpansion@users.noreply.github.com>
2026-09-12 12:54:03 +0530

    0189-ROTATE-ARRAY Stats: Time: 8 ms (13.59%), Space: 262.9 MB (46.59%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,10 +1,23 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
+        // Brute force
         int n = nums.size();
+        k = k % n;
+        vector<int> temp;
+        for(int i = n-k; i < n; i++) {
+            temp.push_back(nums[i]);
+        }
+        for(int i = n-k-1; i >= 0; i--) {
+            nums[i+k] = nums[i];
+        }
+        for(int i = 0; i < k; i++) {
+            nums[i] = temp[i];
+        }
+        /*int n = nums.size();
         k = k % n; 
         reverse(nums.begin(), nums.end());         
         reverse(nums.begin(), nums.begin() + k);   
-        reverse(nums.begin() + k, nums.end());
+        reverse(nums.begin() + k, nums.end());*/
     }
 };
