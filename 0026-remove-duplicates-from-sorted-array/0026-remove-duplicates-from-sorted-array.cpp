7069bd2d279baa7c4d6c1c22315d68b38c71351f
Maikelele <85241561+Domain3xpansion@users.noreply.github.com>
2026-09-12 10:09:18 +0530

    0026-REMOVE-DUPLICATES-FROM-SORTED-ARRAY Stats: Time: 3 ms (11.72%), Space: 23.6 MB (5.58%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,7 +1,18 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
+        // Brute force
+        set<int> uniqueElements;
+        for(int val:nums){
+            uniqueElements.insert(val);
+        }
         int i = 0;
+        for(int val : uniqueElements){
+            nums[i] = val;
+            i++;
+        }
+        return i;
+        /*int i = 0;
         for(int j=1; j<nums.size(); j++){
             if (nums[j] != nums[i]) {
                 i++;
@@ -9,6 +20,6 @@ public:
             }
         }
         return i+1; //Since i points to the position after the last unique element, the number of unique 
-        // elements is equal to i + 1
+        // elements is equal to i + 1*/
     }
 };
