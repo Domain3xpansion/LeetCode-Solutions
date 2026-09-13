f32a677be86cd779bc029a7eae27cb7b0c0614b5
Maikelele <85241561+Domain3xpansion@users.noreply.github.com>
2026-09-13 09:26:26 +0530

    0268-MISSING-NUMBER Stats: Time: 398 ms (5.01%), Space: 21.8 MB (38.28%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,13 +1,27 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int res = 0;
+        /*int res = 0;
         for(int val: nums){
             res = res ^ val;
         }
         for(int i=0; i<=nums.size(); i++){
             res = res ^ i;
         }
-        return res;
+        return res;*/
+
+        // Brute force
+        for(int i=0; i<=nums.size(); i++){
+            int flag = 0;
+            for(int j=0; j<nums.size(); j++){
+                if(i == nums[j]){
+                    flag = 1;
+                    break;
+                }
+            }
+            if(flag == 0)
+                return i;
+        }
+        return -1;
     }
 };
