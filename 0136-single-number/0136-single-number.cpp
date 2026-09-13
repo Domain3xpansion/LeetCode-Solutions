b05f989240038d8fbccab8cd0e73c01b10f1e13a
Maikelele <85241561+Domain3xpansion@users.noreply.github.com>
2026-09-13 10:04:58 +0530

    0136-SINGLE-NUMBER Stats: Time: 8 ms (16.39%), Space: 24 MB (7.35%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,9 +1,20 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int res = 0;
+        /*int res = 0;
         for(int val: nums)
             res = res ^ val;
-        return res;
+        return res;*/
+
+        // Better approach
+        unordered_map<int, int> hashmap;
+        for(int x: nums){
+            hashmap[x]++;
+        }
+        for(auto val : hashmap){
+            if(val.second == 1)
+                return val.first;
+        }
+        return -1;
     }
 };
