fdc938a0bb8ebaedcecae40cbac3697317fb62f0
Maikelele <85241561+Domain3xpansion@users.noreply.github.com>
2026-09-15 23:12:24 +0530

    0496-NEXT-GREATER-ELEMENT-I Stats: Time: 5 ms (13.5%), Space: 12.3 MB (85.72%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         // Brute force
-        vector<int> res;
+        /*vector<int> res;
         for(int i=0; i<nums1.size(); i++){
             auto it = find(nums2.begin(), nums2.end(), nums1[i]);
             if(it != nums2.end()){
@@ -18,6 +18,23 @@ public:
                 if(!found)
                     res.push_back(-1);
                 }
+        }
+        return res;*/
+        vector<int> res, nge(nums2.size(), 0);     // nge stores the next greater element of every position in nums2
+        stack<int> st;
+        for(int i = nums2.size() - 1; i >= 0; i--){   
+            while(!st.empty() && st.top() <= nums2[i])
+            st.pop();
+            if(st.empty())
+                nge[i] = -1;
+            else
+                nge[i] = st.top();
+            st.push(nums2[i]);
+        }
+        for(int x : nums1){        // O(n1 Ã n2)
+            auto it = find(nums2.begin(), nums2.end(), x);
+            int pos = it - nums2.begin();
+            res.push_back(nge[pos]);
         }
         return res;
     }
