f1263d3318a695e230c6ddec78fb5953faabc343
Maikelele <85241561+Domain3xpansion@users.noreply.github.com>
2026-09-17 15:23:35 +0530

    0957-MINIMUM-ADD-TO-MAKE-PARENTHESES-VALID Stats: Time: 0 ms (100%), Space: 8.3 MB (82.16%) - LeetHub

--- 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.cpp
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.cpp
@@ -2,6 +2,15 @@ class Solution {
 public:
     int minAddToMakeValid(string s) {
         int open = 0, unmatched_closing_parentheses = 0;
+        /* open = unmatched (
+        If the string is like this => "()()", 
+        open variable's transition will be like => +1 -> 0 -> +1 -> 0 and unmatched_closing_parentheses will stay 0. So, 0 + 0 = 0
+
+        and unmatched_closing_parentheses = unmatched )
+        If the string is like this => "))(), 
+        unmatched_closing_parentheses variable's transition will be like => +1 -> +2 and open variable's transition will be like +1 -> 0. So, result = 2 + 0 = 2" 
+
+        Good example = "()))((" */
         for(char c : s){
             if(c == '('){
                 open++;
