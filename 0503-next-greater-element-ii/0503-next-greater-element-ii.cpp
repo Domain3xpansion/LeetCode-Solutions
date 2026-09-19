f61c2248667a302ec7778f878e8d1472bf8aad7b
Maikelele <85241561+Domain3xpansion@users.noreply.github.com>
2026-09-19 22:31:48 +0530

    0503-NEXT-GREATER-ELEMENT-II Stats: Time: 3 ms (74.87%), Space: 28.4 MB (62.93%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
-        vector<int> res;
+        /*vector<int> res;
         int n = nums.size();
         for(int i=0; i<n; i++){
             bool found = false;  
@@ -16,6 +16,20 @@ public:
             if(!found)
                 res.push_back(-1);
         }
-        return res;
+        return res;*/
+
+        int n = nums.size();
+        vector<int> nge(n, 0);
+        stack<int> st;
+        for(int i = 2*n - 1; i >= 0; i--){
+            while(!st.empty() && st.top() <= nums[i%n])
+                st.pop();
+            if(st.empty())
+                nge[i%n] = -1;
+            else
+                nge[i%n] = st.top();
+            st.push(nums[i%n]);
+        }
+        return nge;
     }
 };
