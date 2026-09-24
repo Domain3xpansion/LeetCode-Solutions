b106297486e4845409d328fb059d28f7febe958f
Maikelele <85241561+Domain3xpansion@users.noreply.github.com>
2026-09-24 10:23:37 +0530

    0009-PALINDROME-NUMBER Stats: Time: 4 ms (30.49%), Space: 10.8 MB (11.92%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool isPalindrome(int x) {
         // mathematical approach
-        if(x<0) return false;
+        /*if(x<0) return false;
         long original = x;
         long rev = 0;
         while(x>0){
@@ -10,6 +10,21 @@ public:
             rev = (rev * 10) + dig;
             x /= 10;
         }
-        return original == rev;
+        return original == rev;*/
+
+        // 2 pointers approach
+        if(x < 0) 
+            return false;
+        string str = to_string(x);
+        int left = 0;
+        int right = str.length() - 1;
+        while(left<right){
+            if(str[left] != str[right]){
+                return false; // Mismatch found
+            }
+            left++;
+            right--;
+        }
+        return true;
     }
 };
